d7b62d46b559947bb68cdc2c265bae96091db855
jahzeel-hub <acedavid79@gmail.com>
2024-07-22 18:51:36 -0600

    diseñoooo

--- lib/user/evaluation_page.dart
+++ lib/user/evaluation_page.dart
@@ -1,43 +1,67 @@
+import 'dart:ui';
 import 'package:flutter/material.dart';
 
-class DetailedEvaluationPage extends StatelessWidget {
+class DetailedEvaluationPage extends StatefulWidget {
   final int totalTime;
   final int completedTests;
 
   DetailedEvaluationPage({required this.totalTime, required this.completedTests});
 
+  @override
+  _DetailedEvaluationPageState createState() => _DetailedEvaluationPageState();
+}
+
+class _DetailedEvaluationPageState extends State<DetailedEvaluationPage> {
+  bool showBlurEffect = false;
+
   String getEvaluation() {
-    if (completedTests < 4) {
+    if (widget.completedTests < 4) {
       return 'Faltan pruebas por realizar.';
     }
 
-    if (totalTime < 240) {
-      return 'Leve';
-    } else if (totalTime < 300) {
-      return 'Moderada';
-    } else if (totalTime < 420) {
-      return 'Severa';
+    if (widget.totalTime < 240) {
+      return 'Dislexia Leve';
+    } else if (widget.totalTime < 300) {
+      return 'Dislexia Moderada';
+    } else if (widget.totalTime < 420) {
+      return 'Dislexia Severa';
     } else {
-      return 'Profunda';
+      return 'Dislexia Profunda';
+    }
+  }
+
+  Color getEvaluationColor() {
+    if (widget.completedTests < 4) {
+      return Colors.red;
+    }
+
+    if (widget.totalTime < 240) {
+      return Colors.green;
+    } else if (widget.totalTime < 300) {
+      return Colors.yellow;
+    } else if (widget.totalTime < 420) {
+      return Colors.orange;
+    } else {
+      return Colors.red;
     }
   }
 
   String getEvaluationMessage() {
-    if (completedTests < 4) {
+    if (widget.completedTests < 4) {
       return 'Faltan pruebas por realizar.';
     }
 
-    if (totalTime < 240) {
+    if (widget.totalTime < 240) {
       return 'Dificultades menores con la lectura y la escritura. '
           'Errores ocasionales en la ortografía. '
           'Lectura más lenta de lo normal, pero generalmente comprensible. '
           'Capacidad para compensar las dificultades con estrategias de aprendizaje.';
-    } else if (totalTime < 300) {
+    } else if (widget.totalTime < 300) {
       return 'Problemas más consistentes con la lectura y la escritura. '
           'Errores frecuentes en la ortografía y la gramática. '
           'Lectura significativamente más lenta que la media. '
           'Necesidad de apoyo educativo y estrategias de aprendizaje específicas para manejar el trastorno.';
-    } else if (totalTime < 420) {
+    } else if (widget.totalTime < 420) {
       return 'Dificultades graves con la lectura y la escritura. '
           'Errores constantes en la ortografía y la gramática. '
           'Lectura extremadamente lenta y con poca comprensión. '
@@ -59,95 +83,166 @@ class DetailedEvaluationPage extends StatelessWidget {
   }
 
   @override
-  Widget build(BuildContext context) {
-    final evaluation = getEvaluation();
-    final evaluationMessage = getEvaluationMessage();
-    final formattedTime = formatTime(totalTime);
-
+  void initState() {
+    super.initState();
     WidgetsBinding.instance.addPostFrameCallback((_) {
       _showWarningDialog(context);
     });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final evaluation = getEvaluation();
+    final evaluationColor = getEvaluationColor();
+    final evaluationMessage = getEvaluationMessage();
+    final formattedTime = formatTime(widget.totalTime);
 
     return Scaffold(
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                'PRE-DIAGNÓSTICO',
-                style: TextStyle(
-                  fontSize: 28.0,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.teal.shade700,
-                  fontFamily: 'Arial',
-                ),
-              ),
-              SizedBox(height: 20),
-              Text(
-                'Resultado de Evaluación',
-                style: TextStyle(
-                  fontSize: 24.0,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.teal.shade700,
-                  fontFamily: 'Arial',
-                ),
-              ),
-              SizedBox(height: 20),
-              Text(
-                evaluation,
-                style: TextStyle(
-                  fontSize: 22.0,
-                  fontWeight: FontWeight.bold,
-                  color: completedTests < 4 ? Colors.red : Colors.black,
-                  fontFamily: 'Arial',
-                ),
-              ),
-              SizedBox(height: 20),
-              Text(
-                evaluationMessage,
-                style: TextStyle(
-                  fontSize: 18.0,
-                  color: completedTests < 4 ? Colors.red : Colors.black,
-                  fontFamily: 'Arial',
-                ),
-                textAlign: TextAlign.center,
-              ),
-              if (completedTests >= 4) ...[
-                SizedBox(height: 20),
-                Text(
-                  'Tiempo Total: $formattedTime',
-                  style: TextStyle(
-                    fontSize: 18.0,
-                    fontFamily: 'Arial',
-                    fontWeight: FontWeight.bold,
+      body: Stack(
+        children: [
+          SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  SizedBox(height: 40),
+                  Row(
+                    children: [
+                      IconButton(
+                        icon: Icon(Icons.arrow_back, color: Colors.teal.shade700),
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
+                      ),
+                      Expanded(
+                        child: Center(
+                          child: Text(
+                            'PRE-DIAGNÓSTICO',
+                            style: TextStyle(
+                              fontSize: 28.0,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.teal.shade700,
+                              fontFamily: 'Arial',
+                            ),
+                          ),
+                        ),
+                      ),
+                      SizedBox(width: 48),
+                    ],
                   ),
-                ),
-              ],
-              SizedBox(height: 40),
-              ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.blue,
-                  textStyle: TextStyle(fontSize: 18),
-                  minimumSize: Size(200, 50), // Hacer el botón más grande
-                ),
-                onPressed: () {
-                  Navigator.pop(context);
-                },
-                child: Text('Volver al Menú'),
+                  SizedBox(height: 200), // Aumenta el espacio superior
+                  Center(
+                    child: Card(
+                      color: Colors.teal.shade50,
+                      elevation: 5,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(12.0),
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.all(16.0),
+                        child: Column(
+                          children: [
+                            Text(
+                              'Resultado de Evaluación',
+                              style: TextStyle(
+                                fontSize: 24.0,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.teal.shade700,
+                                fontFamily: 'Arial',
+                              ),
+                            ),
+                            SizedBox(height: 20),
+                            Text(
+                              evaluation,
+                              style: TextStyle(
+                                fontSize: 22.0,
+                                fontWeight: FontWeight.bold,
+                                color: evaluationColor,
+                                fontFamily: 'Arial',
+                              ),
+                            ),
+                            SizedBox(height: 20),
+                            Text(
+                              evaluationMessage,
+                              style: TextStyle(
+                                fontSize: 18.0,
+                                color: widget.completedTests < 4 ? Colors.red : Colors.black,
+                                fontFamily: 'Arial',
+                              ),
+                              textAlign: TextAlign.center,
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                  if (widget.completedTests >= 4) ...[
+                    SizedBox(height: 20),
+                    ExpansionTile(
+                      title: Text(
+                        'Ver Tiempo Total',
+                        style: TextStyle(
+                          fontSize: 18.0,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.teal.shade700,
+                        ),
+                      ),
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Text(
+                            'Tiempo Total: $formattedTime',
+                            style: TextStyle(
+                              fontSize: 18.0,
+                              fontFamily: 'Arial',
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                  SizedBox(height: 40),
+                  Center(
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Color.fromARGB(255, 122, 213, 148),
+                        textStyle: TextStyle(fontSize: 18),
+                        minimumSize: Size(140, 60), // Hacer el botón más corto y alto
+                      ),
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                      child: Text('Volver al Menú'),
+                    ),
+                  ),
+                ],
               ),
-            ],
+            ),
           ),
-        ),
+          if (showBlurEffect)
+            Positioned.fill(
+              child: BackdropFilter(
+                filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
+                child: Container(
+                  color: Colors.black.withOpacity(0.2),
+                ),
+              ),
+            ),
+        ],
       ),
     );
   }
 
   void _showWarningDialog(BuildContext context) {
+    setState(() {
+      showBlurEffect = true;
+    });
+
     showDialog(
       context: context,
-      barrierDismissible: false, // Evita que se pueda cerrar el diálogo haciendo clic fuera de él
+      barrierDismissible: false,
       builder: (BuildContext context) {
         return AlertDialog(
           shape: RoundedRectangleBorder(
@@ -199,7 +294,7 @@ class DetailedEvaluationPage extends StatelessWidget {
                       ),
                     ),
                     onPressed: () {
-                      Navigator.pop(context); // Cierra el diálogo y continúa
+                      Navigator.of(context).pop(); // Cierra el diálogo
                     },
                   ),
                   SizedBox(height: 10),
@@ -220,8 +315,8 @@ class DetailedEvaluationPage extends StatelessWidget {
                       ),
                     ),
                     onPressed: () {
-                      Navigator.pop(context);
-                      Navigator.pop(context); // Cierra el diálogo y regresa al menú
+                      Navigator.of(context).pop(); // Cierra el diálogo
+                      Navigator.of(context).pop(); // Regresa a la página anterior
                     },
                   ),
                 ],
@@ -230,6 +325,12 @@ class DetailedEvaluationPage extends StatelessWidget {
           ],
         );
       },
-    );
+    ).then((_) {
+      if (mounted) {
+        setState(() {
+          showBlurEffect = false;
+        });
+      }
+    });
   }
 }
